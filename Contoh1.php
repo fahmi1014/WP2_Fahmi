f2c3e82fe9c75f86960e7eeaf4cbe09c7666ed44
fahmi1014 <105929650+fahmi1014@users.noreply.github.com>
2022-05-20 20:57:59 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "perkenalan<br>";
+    echo "nama saya fahmi.saya seorang mahasiswa bsi<>";
+    echo "saya suka bermain futsal";
+    }
+}
